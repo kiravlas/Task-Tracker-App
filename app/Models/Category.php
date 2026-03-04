a38b4b540fe27df6b81bf694ff0d5dfe53afa914
Chiril Vlasenco <121399822+kiravlas@users.noreply.github.com>
2026-03-04 03:04:16 +0200

    Set up relationships between Task, Category, User Models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks(): HasMany {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected function casts(): array
+    {
+        return [
+            'is_recurring' => 'boolean',
+            'completed_at' => 'datetime',
+            'task_date' => 'datetime',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
