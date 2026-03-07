e0ac7c69f60d715f5865711f28e54d7473b14e74
Chiril Vlasenco <121399822+kiravlas@users.noreply.github.com>
2026-03-07 05:02:31 +0200

    Add Rate Limiterfor login attempts

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -19,12 +21,22 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
+
+        $throttleKey = strtolower($request->input('email')).'|'.$request->ip();
+        if (RateLimiter::tooManyAttempts($throttleKey, 5)) {
+            throw ValidationException::withMessages(['email' => 'Too many login attempts. Please try again in 5 minutes.']);
+        }
+
         if (Auth::attempt($request->only(['email', 'password']), $request->boolean('remember'))) {
             $request->session()->regenerate();
+
+            RateLimiter::clear($throttleKey);
 
             return redirect()->intended(route('dashboard', absolute: false));
         }
 
-        return back()->withErrors(['email' => 'These credentials do not match our records']);
+        RateLimiter::hit($throttleKey);
+
+        throw ValidationException::withMessages(['email' => 'These credentials do not match our records']);
     }
 }
